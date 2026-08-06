f63bb11558ef9ccecd949ebc46efb7796d903c50
mustafa-muhamma <shakhsiamohamedabelmaguidgttzv@gmail.com>
2026-08-06 16:15:36 +0300

    feat(public): wire About section into home page

--- client/src/app/(public)/page.tsx
+++ client/src/app/(public)/page.tsx
@@ -1,9 +1,11 @@
+import { About } from "@/components/public/about";
 import { Hero } from "@/components/public/hero";
 
 export default function PublicPage() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
